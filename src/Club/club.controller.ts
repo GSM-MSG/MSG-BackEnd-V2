940fb45af3163c60d9b5308559d8d3bb40a669fa
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-03-20 23:07:54 +0900

    Add list

--- src/Club/club.controller.ts
+++ src/Club/club.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ClubService } from './club.service';
 
 @Controller('club')
-export class ClubController {}
+export class ClubController {
+  constructor(private clubService: ClubService) {}
+  @Get('/list')
+  async list(@Query('type') clubType: string) {
+    return await this.clubService.list(clubType);
+  }
+}
--- src/Club/club.service.ts
+++ src/Club/club.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Club } from 'src/Entities/Club.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ClubService {}
+export class ClubService {
+  constructor(@InjectRepository(Club) private club: Repository<Club>) {}
+  async list(clubType: string) {
+    if (clubType === 'MAJOR' || 'EDITORIAL' || 'FREEDOM') {
+      return this.club.find({ where: { type: clubType } });
+    } else {
+      throw new HttpException(
+        '동아리타입이 잘못되었습니다.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
