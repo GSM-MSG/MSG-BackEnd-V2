d59a7060cf2cb05ce30537da65aadcf1229f426b
변찬우 <chan00106@naver.com>
2022-03-24 17:48:17 +0900

    Update check router

--- src/check/check.controller.ts
+++ src/check/check.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
+import { Controller, Get, HttpCode, UseGuards } from '@nestjs/common';
+import { RtGuard } from '../auth/guards';
+import { Public } from '../auth/decorators';
 
 @Controller('check')
 export class CheckController {
   @Get('/')
   @HttpCode(200)
+  @Public()
+  @UseGuards(new RtGuard())
   check() {
     return;
   }
