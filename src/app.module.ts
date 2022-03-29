90c4bb4b337b9253b4d4c4f498ad96a1d1c36a6b
변찬우 <chan00106@naver.com>
2022-03-29 20:23:17 +0900

    :bug: Remove entities import
    conflict 해결 잘못한거 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClubModule } from './club/club.module';
 import entities from './Entities';
-
 import { AuthModule } from './auth/auth.module';
 import { CheckModule } from './check/check.module';
-import entities from './Entities';
 import { AtStrategy } from './strategies/atStrategy';
 import { RtStrategy } from './strategies/rtStrategy';
 
