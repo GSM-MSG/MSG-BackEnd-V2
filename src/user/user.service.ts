0fa0b50ddc1aa8bb2610168a69ea190a52ff21ee
시훈 <81404026+sihun96@users.noreply.github.com>
2022-04-12 16:23:12 +0900

    Add get User Data

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,4 +6,14 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class UserService {
   constructor(@InjectRepository(User) private user: Repository<User>) {}
+
+  async getUserData(email: string) {
+    const userData = await this.user.findOne(
+      { email: email },
+      { relations: ['requestJoin', 'requestJoin.clubId'] },
+    );
+    delete userData.refreshToken;
+    delete userData.password;
+    return userData;
+  }
 }
