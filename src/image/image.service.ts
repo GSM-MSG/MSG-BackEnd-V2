bd50a6d67f2805cf1e671aed62a2c3e1aa725d19
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-04-18 18:17:08 +0900

    :sparkles: s3서비스 생성

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -5,17 +5,18 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class ImageService {
   constructor(private configService: ConfigService) {}
-  AWS_S3_BUCKET = process.env.AWS_BUCKET; //추후 버킷으로 변경
+  AWS_S3_BUCKET = this.configService.get<string>('AWS_BUCKET'); //추후 버킷으로 변경
   s3 = new AWS.S3({
     accessKeyId: process.env.AWSAccessKeyId,
     secretAccessKey: process.env.AWSSecretKey,
   });
 
   async uploladFile(files: Array<Express.Multer.File>) {
+    const list = [];
     files.forEach(async (element) => {
-      const param = {
+      const params = {
         Bucket: this.AWS_S3_BUCKET,
-        key: element.originalname,
+        Key: element.originalname,
         Body: element.buffer,
         ACL: 'public-read',
         ContenctType: element.mimetype,
@@ -25,7 +26,10 @@ export class ImageService {
         },
       };
       try {
+        const result = this.s3.upload(params).promise().then();
+        list.push((await result).Location);
       } catch (e) {}
     });
+    return list;
   }
 }
