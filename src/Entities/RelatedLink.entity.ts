b244ccfe56e5785728c7fe3a03fb84d7f9f80f48
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-04-20 17:08:14 +0900

    :poop: relatedlinkentity 관계 수정

--- src/Entities/RelatedLink.entity.ts
+++ src/Entities/RelatedLink.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Club } from './Club.entity';
 
 @Entity()
@@ -12,6 +12,6 @@ export class RelatedLink {
   @Column()
   url: string;
 
-  @OneToMany(() => Club, (club) => club.id, { onDelete: 'CASCADE' })
+  @ManyToOne(() => Club, (club) => club.id, { onDelete: 'CASCADE' })
   club: Club;
 }
