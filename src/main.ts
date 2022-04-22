99f184ec1733263fca8fff1e2475b87c3e2fbdd9
시훈 <81404026+sihun96@users.noreply.github.com>
2022-04-22 11:03:50 +0900

    :bug: npm설치가 잘못된 버그 수정

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,24 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { AtGuard } from './auth/guards';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
   });
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalGuards(new AtGuard(new Reflector()));
+
+
+
   await app.listen(4000);
 }
 bootstrap();
