17f530bf2b4b0fd1f5c78413c78f579ec54fb824
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-04-22 13:15:04 +0900

    Update at & rt strategy

--- src/strategies/rtStrategy.ts
+++ src/strategies/rtStrategy.ts
@@ -3,10 +3,16 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { Request } from 'express';
 import { Injectable } from '@nestjs/common';
 import { refreshToken } from 'src/lib/Constants';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/Entities/User.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
-  constructor() {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: refreshToken,
@@ -14,8 +20,10 @@ export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
     });
   }
 
-  validate(req: Request, payload: any) {
+  async validate(req: Request, payload: any) {
     const refreshToken = req.get('authorization').replace('Bearer', '').trim();
+    const user = await this.userRepository.findOne(payload.email);
+    if (!user || !bcrypt.compare(refreshToken, user.refreshToken)) return false;
     return { ...payload, refreshToken };
   }
 }
