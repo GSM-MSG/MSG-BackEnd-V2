0da579033aecefdb098c35d352b97469b4e57145
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-04-25 15:49:20 +0900

    ⚡️ Update emailService
    스펨함으로 가는 문제 해결

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,38 +1,46 @@
 import { Injectable, Logger } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
-import Mail from 'nodemailer/lib/mailer';
-import { EmailOptions } from 'src/types/EmailOptions';
-import { EmailHtml } from 'src/lib/EmailHtml';
-import { MAIL, PASS } from 'src/lib/EmailLib';
+import { google } from 'googleapis';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EmailService {
-  private transporter: Mail;
+  private Google = new google.auth.OAuth2(
+    this.configService.get('CLIENT_ID'),
+    this.configService.get('CLIENT_PW'),
+  );
 
-  constructor() {
-    this.transporter = nodemailer.createTransport({
-      service: 'naver',
-      host: 'smtp.naver.com',
-      port: 587,
-      auth: {
-        user: MAIL,
-        pass: PASS,
-      },
-      from: MAIL,
+  constructor(private configService: ConfigService) {
+    this.Google.setCredentials({
+      refresh_token: this.configService.get('REFRESH'),
     });
   }
 
   async userVerify(emailAddress: string, code: string) {
-    const mailOptions: EmailOptions = {
-      from: MAIL,
+    const accessToken = await this.Google.getAccessToken();
+    const transporter = this.getTransporter(accessToken);
+    const mailOptions = {
+      from: this.configService.get('CLIENT_EMAIL'),
       to: emailAddress,
-      subject: 'GCMS 가입 인증 메일',
-      html: EmailHtml(code),
+      subject: 'Hello, GSM test 메일',
+      html: `${code}`,
     };
 
-    const result: { accepted: string[] } = await this.transporter.sendMail(
-      mailOptions,
-    );
+    const result = await transporter.sendMail(mailOptions);
     Logger.log(`Send mail to ${result.accepted[0]}`);
+  }
+
+  private getTransporter(accessToken: any) {
+    return nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: this.configService.get('CLIENT_EMAIL'),
+        clientId: this.configService.get('CLIENT_ID'),
+        clientSecret: this.configService.get('CLIENT_PW'),
+        refreshToken: this.configService.get('REFRESH'),
+        accessToken,
+      },
+    });
   }
 }
