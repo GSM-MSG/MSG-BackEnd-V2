f94c124e2bf2de2c61174647fc04570444bb75b8
시훈 <81404026+sihun96@users.noreply.github.com>
2022-04-28 14:32:20 +0900

    :sparkles: 사진업로드 설명 추가

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -6,12 +6,26 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ImageService } from './image.service';
-
+@ApiTags('IMAGE')
 @Controller('image')
 export class ImageController {
   constructor(private imageService: ImageService) {}
 
+  @ApiBearerAuth('access-token')
+  @ApiOperation({
+    summary: '사진 업로드',
+    description: '사진을 S3에 업로드합니다',
+  })
+  @ApiBody({ isArray: true })
+  @ApiResponse({ status: 200, description: 's3주소 반환' })
   @Post('')
   @UseInterceptors(FilesInterceptor('files'))
   @Bind(UploadedFiles())
