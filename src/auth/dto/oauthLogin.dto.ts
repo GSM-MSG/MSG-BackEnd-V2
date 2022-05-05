26e42413a8d5564eabc0bda089d18886a822341b
baegteun <baegteun@gmail.com>
2022-05-05 23:37:15 +0900

    :fire: :: unused import

--- src/auth/dto/oauthLogin.dto.ts
+++ src/auth/dto/oauthLogin.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty, ApiResponse } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class OauthMobileLoginDto {
