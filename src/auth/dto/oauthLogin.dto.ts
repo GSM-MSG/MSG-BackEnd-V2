948b9874cd39bf3b4261b183579d04807acd3db6
baegteun <baegteun@gmail.com>
2022-05-05 23:35:29 +0900

    :memo: :: DTO에 Swagger작업을 했습니다.

--- src/auth/dto/oauthLogin.dto.ts
+++ src/auth/dto/oauthLogin.dto.ts
@@ -1,9 +1,20 @@
+import { ApiProperty, ApiResponse } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class OauthMobileLoginDto {
+  @ApiProperty({
+    example: 'GOOGLE_CLIENT_ID',
+    description: '해당 플랫폼의 CLIENT ID',
+    required: true,
+  })
   @IsString()
   clientId: string;
 
+  @ApiProperty({
+    example: 'A.B.C',
+    description: '모바일 클라이언트에서 OAuth2.0 인증 시 전달되는 유저의 정보',
+    required: true,
+  })
   @IsString()
   idToken: string;
 }
