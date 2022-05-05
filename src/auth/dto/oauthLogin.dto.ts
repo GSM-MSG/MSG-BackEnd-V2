83338c2bb1ef61211ab918d5a5d0c3b3390b48e3
baegteun <baegteun@gmail.com>
2022-05-05 23:56:42 +0900

    :fire: :: clientId

--- src/auth/dto/oauthLogin.dto.ts
+++ src/auth/dto/oauthLogin.dto.ts
@@ -1,15 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsJWT, IsString } from 'class-validator';
+import { IsJWT } from 'class-validator';
 
 export class OauthMobileLoginDto {
-  @ApiProperty({
-    example: 'GOOGLE_CLIENT_ID',
-    description: '해당 플랫폼의 CLIENT ID',
-    required: true,
-  })
-  @IsString()
-  clientId: string;
-
   @ApiProperty({
     example: 'A.B.C',
     description: '모바일 클라이언트에서 OAuth2.0 인증 시 전달되는 유저의 정보',
