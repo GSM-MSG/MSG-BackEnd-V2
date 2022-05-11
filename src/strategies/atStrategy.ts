315f2c8b10000a7e5d269bfba702c5759d1b7073
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-05-11 19:48:02 +0900

    ✨ Update authorization -> cookie
    모든 인가를 cookie로 함

--- src/strategies/atStrategy.ts
+++ src/strategies/atStrategy.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/Entities/User.entity';
 import { Repository } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
 
 type JwtPayload = {
   email: string;
@@ -17,7 +18,13 @@ export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
   ) {
     const configService = new ConfigService();
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          const cookie = req.cookies['accessToken'];
+          if (!cookie) return null;
+          return cookie;
+        },
+      ]),
       secretOrKey: configService.get('ACCESS_TOKEN_SECRET'),
     });
   }
--- src/strategies/rtStrategy.ts
+++ src/strategies/rtStrategy.ts
@@ -15,7 +15,13 @@ export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
     private readonly configService: ConfigService,
   ) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          const cookie = req.cookies['accessToken'];
+          if (!cookie) return null;
+          return cookie;
+        },
+      ]),
       secretOrKey: configService.get('REFRESH_TOKEN_SECRET'),
       passReqToCallback: true,
     });
