6b9b40b97cda9d3a28ca6223a9cfef0ad35813aa
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-05-19 16:48:53 +0900

    🐛 Update 파일명
    jibinBug 해결

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -17,7 +17,7 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { User } from 'src/auth/decorators';
-import { clubDatadto } from 'src/Club/dto/clubData.dto';
+import { clubDatadto } from 'src/Club/dto/ClubData.dto';
 import { urlDto } from './dto/urlAddress.dto';
 import { UserService } from './user.service';
 
