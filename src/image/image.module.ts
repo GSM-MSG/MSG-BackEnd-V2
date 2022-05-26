0cf6ad5710ccbe67582242cb99db8092eb4d8c01
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-05-26 11:51:31 +0900

    :zap: imagecontroller 추가
    ㅇㅇ

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { ImageController } from './image.controller';
+import { ImageWebController } from './image.web.controller';
 
 @Module({
   providers: [ImageService],
-  controllers: [ImageController],
+  controllers: [ImageController, ImageWebController],
 })
 export class ImageModule {}
