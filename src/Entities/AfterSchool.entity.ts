e43c0d69a389538f7ec5c4c8f8614dc8893ebc74
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-05-26 18:24:17 +0900

    :sparkles: afterschoolEntity 생성
    ㅇㅇ

---
+++ src/Entities/AfterSchool.entity.ts
@@ -0,0 +1,39 @@
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class AfterSchool {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  personnel: number;
+
+  @Column()
+  week: string;
+
+  @Column()
+  class: number;
+
+  @Column()
+  teacher: string;
+
+  @Column()
+  canDuplicate: boolean;
+
+  @Column()
+  isCommon: boolean;
+
+  @Column()
+  max_personnel: number;
+
+  @Column()
+  full: boolean;
+
+  @Column()
+  season: string;
+
+  @Column()
+  year: number;
+}
