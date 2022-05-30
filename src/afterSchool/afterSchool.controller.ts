38beb46f1502dbcf2df45b84e835d0ce00873d7b
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-05-30 16:08:48 +0900

    :sparkles: 방과후 지원이 된다..!
    git push

--- src/afterSchool/afterSchool.controller.ts
+++ src/afterSchool/afterSchool.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from 'src/auth/decorators';
+import { AfterSchoolService } from './afterSchool.service';
+import { ApplyAfterSchoolDto } from './dto/ApplyAfterSchool.dto';
 
-@Controller('after-school')
-export class AfterSchoolController {}
+@Controller('afterSchool')
+export class AfterSchoolController {
+  constructor(private afterSchoolService: AfterSchoolService) {}
+
+  @Post('apply')
+  async applyAfterSchool(
+    @Body() ApplyAfterSchoolDto: ApplyAfterSchoolDto,
+    @User('email') email: string,
+  ) {
+    this.afterSchoolService.applyAfterSchool(ApplyAfterSchoolDto, email);
+  }
+}
