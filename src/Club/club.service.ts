f20ab7d62090d86a9d30ed540eaaaba7aae3d653
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-06-09 11:49:56 +0900

    :sparkles: 동아리를 이미 생선한 유저거르기
    dd

--- src/Club/club.service.ts
+++ src/Club/club.service.ts
@@ -71,7 +71,8 @@ export class ClubService {
     const check = checkUser.filter((member) => {
       return member.club.type === type;
     });
-    if (check) {
+    console.log(check);
+    if (check[0]) {
       throw new HttpException(
         '이미 동아리를 만든 유저입니다.',
         HttpStatus.BAD_REQUEST,
