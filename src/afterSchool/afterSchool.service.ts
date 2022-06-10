63feceeb3a8130d2363d2e10224cf1a08961e381
Huuuunee <s21024@gsm.hs.kr>
2022-06-10 10:16:41 +0900

    :sparkles: 중간

--- src/afterSchool/afterSchool.service.ts
+++ src/afterSchool/afterSchool.service.ts
@@ -6,6 +6,8 @@ import { ClassRegistration } from 'src/Entities/ClassRegistration.entity';
 import { User } from 'src/Entities/User.entity';
 import { Repository } from 'typeorm';
 import { ApplyAfterSchoolDto } from './dto/ApplyAfterSchool.dto';
+import { DayOfWeek } from 'src/Entities/DayOfWeek.entity';
+import { Grade } from 'src/Entities/Grade.entity';
 
 @Injectable()
 export class AfterSchoolService {
@@ -14,8 +16,30 @@ export class AfterSchoolService {
     @InjectRepository(User) private user: Repository<User>,
     @InjectRepository(ClassRegistration)
     private classRegistration: Repository<ClassRegistration>,
+    @InjectRepository(DayOfWeek) private dayOfWeek: Repository<DayOfWeek>,
+    @InjectRepository(Grade) private grade: Repository<Grade>,
   ) {}
-  async list(listDataDto: ListDataDto, email: string) {}
+  async list(listDataDto: ListDataDto, email: string) {
+    const weekData = await this.dayOfWeek.find({
+      where: { dayOfWeek: listDataDto.week },
+    });
+    const gradeData = await this.grade.find({
+      where: { grade: listDataDto.grade },
+    });
+    const afterSchoolData = await this.afterSchool.find({
+      where: {
+        season: listDataDto.season,
+        dayOfWeek: weekData,
+        grade: gradeData,
+      },
+    });
+    let ASDJ = new Object();
+    ASDJ[0] = { ...afterSchoolData };
+    afterSchoolData.forEach((data) => {
+      return -1;
+    });
+    console.log(ASDJ[0]);
+  }
   async applyAfterSchool(applyAfterSchool: ApplyAfterSchoolDto, email: string) {
     const afterSchoolData = await this.afterSchool.findOne({
       where: { id: applyAfterSchool.afterSchoolId },
