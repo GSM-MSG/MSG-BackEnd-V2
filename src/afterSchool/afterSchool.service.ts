d58e996ec6d5dc7c544e2a6bf61781c563f4d0b3
Huuuunee <s21024@gsm.hs.kr>
2022-06-10 10:17:36 +0900

    :fire: 중복 삭제

--- src/afterSchool/afterSchool.service.ts
+++ src/afterSchool/afterSchool.service.ts
@@ -8,16 +8,12 @@ import { Grade } from 'src/Entities/Grade.entity';
 import { User } from 'src/Entities/User.entity';
 import { Repository } from 'typeorm';
 import { ApplyAfterSchoolDto } from './dto/ApplyAfterSchool.dto';
-import { DayOfWeek } from 'src/Entities/DayOfWeek.entity';
-import { Grade } from 'src/Entities/Grade.entity';
 
 @Injectable()
 export class AfterSchoolService {
   constructor(
     @InjectRepository(AfterSchool) private afterSchool: Repository<AfterSchool>,
     @InjectRepository(User) private user: Repository<User>,
-    @InjectRepository(DayOfWeek) private dayOfWeek: Repository<DayOfWeek>,
-    @InjectRepository(Grade) private grade: Repository<Grade>,
     @InjectRepository(ClassRegistration)
     private classRegistration: Repository<ClassRegistration>,
     @InjectRepository(DayOfWeek) private dayOfWeek: Repository<DayOfWeek>,
