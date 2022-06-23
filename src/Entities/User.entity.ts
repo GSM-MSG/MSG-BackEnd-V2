6e8ae8c9415dd4766cde11e2d25f17ce11139c5f
Huuuunee <s21024@gsm.hs.kr>
2022-06-24 08:33:34 +0900

    :bug: 매핑이 user가 아닌 id로 되있는것 수정

--- src/Entities/User.entity.ts
+++ src/Entities/User.entity.ts
@@ -31,7 +31,7 @@ export class User {
 
   @OneToMany(
     () => ClassRegistration,
-    (ClassRegistration) => ClassRegistration.id,
+    (ClassRegistration) => ClassRegistration.user,
   )
   classRegistration: ClassRegistration[];
 
