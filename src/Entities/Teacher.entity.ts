a0764e9b68f304719f6391a5b5a05242cf524aa5
Huuuunee <s21024@gsm.hs.kr>
2022-06-27 09:57:11 +0900

    :sparkles: 선생님 엔티티 추가

---
+++ src/Entities/Teacher.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class Teacher {
+    @PrimaryColumn()
+    userId: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    name: string;
+}
