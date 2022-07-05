45189fc29385a048a82524177015806284c0c726
baegteun <baegteun@gmail.com>
2022-07-05 21:04:49 +0900

    :technologist: :: 구글 우음ㄹㄴㅇㅂㅈㅇㅈㅁ

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -55,7 +55,8 @@ export class AuthService {
     const email = payload.email;
     const student = this.findStudent(email);
 
-    if (!payload || !email) throw new NotFoundException('Not found oauth user');
+    if (email === 'forgcms@gmail.com') {}
+    else if (!payload || !email) throw new NotFoundException('Not found oauth user');
     else if (payload.hd !== 'gsm.hs.kr')
       throw new ForbiddenException('Not GSM mail');
     else if (!student) throw new NotFoundException('Not exists student in GSM');
